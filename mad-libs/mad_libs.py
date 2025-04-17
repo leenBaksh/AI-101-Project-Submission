c4e4cb4a4ea918378b989809f7b30ebef6e9a58c
FaybieBakshi <filzawaseem086@gmail.com>
2025-04-18 03:42:53 +0500

    first commit

---
+++ mad-libs/mad_libs.py
@@ -0,0 +1,15 @@
+# Mad Libs Project
+
+print("Let's create a fun Mad Libs story!")
+
+adj = input("Enter another adjective: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+famous_person = input("Famous peron: ")
+
+story = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print("\nHere's your story:")
+print(story)
+
